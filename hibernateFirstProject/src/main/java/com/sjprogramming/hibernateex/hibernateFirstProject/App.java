a729eaef141a2396e6652425f593671e138c7a88
surya prakash <74907025+surya-vats@users.noreply.github.com>
2024-06-23 15:38:07 +0530

    Add files via upload

---
+++ hibernateFirstProject/src/main/java/com/sjprogramming/hibernateex/hibernateFirstProject/App.java
@@ -0,0 +1,13 @@
+package com.sjprogramming.hibernateex.hibernateFirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
